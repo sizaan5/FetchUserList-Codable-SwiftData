06131605a8039be7a291101d1cbacf15a68e1e46
Izaan Saleem <sizaan55@gmail.com>
2024-09-10 11:12:48 +0500

    first commit

---
+++ FetchUserList-Codable-SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchUserList-Codable-SwiftData
+//
+//  Created by Izaan Saleem on 10/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchUserList-Codable-SwiftData/FetchUserList_Codable_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchUserList_Codable_SwiftDataApp.swift
+//  FetchUserList-Codable-SwiftData
+//
+//  Created by Izaan Saleem on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FetchUserList_Codable_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
